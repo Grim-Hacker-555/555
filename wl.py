a5765441f77b99b96c07aa1358e36bf812c1d938
Grim Hacker 555 <51239639+Grim-Hacker-555@users.noreply.github.com>
2020-01-22 16:16:27 +0700

    Add files via upload

---
+++ wl.py
@@ -0,0 +1,15 @@
+import os,sys
+os.system("clear && figlet hi world")
+a = input("  Enter the starting number : ")
+print ""
+b = input("  Enter the last number : ")
+print ""
+c = raw_input("  Enter Password List file : ")
+os.system("clear")
+
+while True :
+	print a
+	a += 1
+	if a == b :
+		sys.exit("Password List successfully")
+	os.system("echo %s >> %s" %(a,c))
